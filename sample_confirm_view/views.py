2ac1a6a6d2fdfb6aa46ff4e5311f653668e676d3
nakamatsu <nakamatsu@hands-inc.co.jp>
2022-07-09 13:33:17 +0900

    確認画面サンプル１

--- sample_confirm_view/views.py
+++ sample_confirm_view/views.py
@@ -7,14 +7,18 @@ class Sample1View(FormView):
     success_url = reverse_lazy('sample_confirm_view:sample1_complete')
 
     def get_template_names(self):
-        if self.request.POST.get('step') == 'preview':
-            return 'sample_confirm_view/sample1/confirm.html'
+        if self.request.POST.get('step') == 'confirm':
+            return ['sample_confirm_view/sample1/confirm.html']
         else:
-            return 'sample_confirm_view/sample1/form.html'
+            return ['sample_confirm_view/sample1/form.html']
 
     def form_valid(self, form):
         if self.request.POST['step'] != 'complete':
             return self.render_to_response(self.get_context_data(form=form))
+        
+        ## ここでデータベースに保存
+        # Booking.objects.create()
+
         return super().form_valid(form)
 
 
