b2bce1fcdc5ff339b7a3d767d1a89c3a4251a924
KOTA <sakamichi.hinatazaka.46@gmail.com>
2022-12-17 12:17:21 +0900

    レッスン終了後の最新コード

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,10 @@ from app import views
 urlpatterns = [
     path('', views.HotelView.as_view(), name='hotel'),
     path('hotel/<int:pk>/plan/', views.PlanView.as_view(), name='plan'),
-    path('calender', views.CalendarView.as_view(), name='calendar'),
-    path('booking/', views.BookingView.as_view(), name='booking'),
+    path('calendar/<int:pk>', views.CalendarView.as_view(), name='calendar'),
+    path('calendar/<int:pk>/<int:year>/<int:month>/<int:day>', views.CalendarView.as_view(), name='calendar'),
+    # path('calendar', views.CalendarView.as_view(), name='calendar'),
+    path('booking/<int:plan_id>/', views.BookingView.as_view(), name='booking'),
     path('confirm/', views.BookingView.as_view(), name='confirm'),
     path('thanks', views.ThanksView.as_view(), name='thanks'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,21 +1,12 @@
-import email
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, FormView, View
+from django.urls import reverse
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime, date, timedelta, time
 
 from app.forms import BookingForm
 from .models import Hotel, BookingPlan, Booking
 
-
-#class HotelView(View):
-    #def get(self, request, *args, **kwargs):
-        #hotel_data = Hotel.objects.all()
-        
-        #return render(request, 'app/hotel.html', {
-           # 'hotel_data':hotel_data
-        #})
-        
 class HotelView(ListView):
     template_name = 'app/hotel.html'
     model = Hotel
@@ -29,11 +20,12 @@ class PlanView(ListView):
     
 class CalendarView(View):
     def get(self, request, *args, **kwargs):
-        staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
+        planname_data = BookingPlan.objects.filter(id=self.kwargs['pk']).select_related('hotel')[0]
         today = date.today()
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         day = self.kwargs.get('day')
+        
         if year and month and day:
             start_date = date(year=year, month=month, day=day)
         else:
@@ -41,23 +33,7 @@ class CalendarView(View):
         days = [start_date + timedelta(days=day) for day in range(7)]
         start_day = days[0]
         end_day = days[-1]
-        
-        calendar = {}
-        for hour in range(10, 21):
-            row = {}
-            for day in days:
-                row[day] = True
-            calendar[hour] = row
-        start_time = make_aware(datetime.combine(start_day, time(hour=10, minute=0, second=0)))
-        end_time = make_aware(datetime.combine(end_day, time(hour=20, minute=0, second=0)))
-        booking_data = Booking.objects.filter(staff=staff_data).exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
-        for booking in booking_data:
-            local_time = localtime(booking.start)
-            booking_date = local_time.date()
-            booking_hour = local_time.hour
-            if (booking_hour in calendar) and (booking_date in calendar[booking_hour]):
-                calendar[booking_hour][booking_date] = False
-
+        booking_data = BookingPlan.objects.filter(planname=planname_data)
 
         target_date = start_date
         calendar_object = MyLocaleHTMLCalendar(locale='ja_jp', pk=self.kwargs['pk'])
@@ -66,7 +42,8 @@ class CalendarView(View):
 
 
         return render(request, 'app/calendar.html',{
-            'staff_data': staff_data,
+            'planname_data':planname_data,
+            'plan_id': self.kwargs['pk'],
             'calendar': calendar,
             'days': days,
             'start_day': start_day,
@@ -108,7 +85,8 @@ class MyLocaleHTMLCalendar(calendar.LocaleHTMLCalendar):
             # day outside month
             return '<td class="%s">&nbsp;</td>' % self.cssclass_noday
         else:
-            url = reverse('booking', kwargs={'pk': self.pk, 'year': self.theyear, 'month': self.themonth, 'day': day})
+            #url = reverse('booking', kwargs={'pk': self.pk, 'year': self.theyear, 'month': self.themonth, 'day': day})
+            url = reverse('booking', kwargs={'plan_id': self.pk})
             # return '<td class="%s"><a href="%s">%d</a></td>' % (self.cssclasses[weekday], url, day)
             return f'<td class="{self.cssclasses[weekday]}"><a href="{url}">{day}</a></td>'
 
@@ -136,7 +114,7 @@ class BookingView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        plan_id = self.request.GET.get('plan_id')
+        plan_id = self.kwargs.get('plan_id')
         print(plan_id)
         context['plan'] = BookingPlan.objects.get(pk=plan_id)
         return context
